15448e83388b34a8bab83358389f4e85c9179c0c
Jatin Choubey <jatin.choubey@spironet.com>
2024-11-28 13:19:01 +0530

    tested on testnet

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -2,4 +2,4 @@ const SpiroRapidoAgreement = artifacts.require("SpiroRapidoAgreement");
 
 module.exports = function (deployer, network, accounts) {
     deployer.deploy(SpiroRapidoAgreement/*, { from: accounts[0] }*/);
-}
+}   
